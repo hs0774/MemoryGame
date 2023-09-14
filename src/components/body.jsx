038f5bb4fcf5b43c978765c28ee935f805a066ee
Hardeep Singh <hs0774@gmail.com>
2023-09-14 14:41:13 -0400

    had to learn more about state to fix my issue but now it works

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,86 +1,84 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../css/body.css";
-import { unstable_renderSubtreeIntoContainer } from "react-dom";
-import _ from 'lodash';
+import _, { shuffle } from 'lodash';
 
-export default function Body({changeScore,lost}) {
-  const [pokemonData, setPokemonData] = React.useState([]);
+export default function Body({ changeScore, lost }) {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [clickedPokemon, setClickedPokemon] = useState(new Set());
   const pokemon = [149, 248, 306, 330, 373, 376, 445, 612, 635, 637, 706, 715, 784, 887, 998];
 
-    React.useEffect(() => {
-        Promise.all(
-        pokemon.map((dragon,id) => {
-            return fetch(`https://pokeapi.co/api/v2/pokemon/${dragon}/`)
-            .then(response => response.json())
-            .then(data => {
-                 return {
-                    imageUrl:data.sprites.other['official-artwork'].front_default,
-                    name:data.name,
-                    id:pokemon[id],
-                    isClicked:false,
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                return null; 
-            });
-        })
-        ).then(results => {
-        setPokemonData(results); 
-        });
-    }, []);
-  
-    function loseGame() {
-        setPokemonData(prevData => {
-          const newData = prevData.map(data => ({
-            ...data,
-            isClicked: false
-          }));
-          
-          lost();
-      
-          return newData;
-        });
-      }
+  useEffect(() => {
+    Promise.all(
+      pokemon.map((dragon, id) => {
+        return fetch(`https://pokeapi.co/api/v2/pokemon/${dragon}/`)
+          .then(response => response.json())
+          .then(data => {
+            return {
+              imageUrl: data.sprites.other['official-artwork'].front_default,
+              name: data.name,
+              id: pokemon[id],
+              isClicked: false,
+            }
+          })
+          .catch(error => {
+            console.error('Error:', error);
+            return null;
+          });
+      })
+    ).then(results => {
+      setPokemonData(results);
+    });
+  }, []);
 
-      
-    function handleClick(id) {
-        let gameLost = false;
-        setPokemonData(prevData =>
-            prevData.map(data => {
-                if (id === data.id) {
-                    if(data.isClicked === true){
-                        loseGame();
-                        gameLost = true;
-                    } else {
-                    return {
-                        ...data,
-                        isClicked: true
-                    };
-                    }
-                }
-                return data;
-            })
-        );
+  function loseGame() {
+    setPokemonData(prevData => {
+      const newData = prevData.map(data => ({
+        ...data,
+        isClicked: false
+      }));
 
-        if (!gameLost) {
-            changeScore();
+      lost();
+
+      return newData;
+    });
+    setClickedPokemon(new Set());
+    shuffleCards();
+  }
+
+  function handleClick(id) {
+    if (clickedPokemon.has(id)) {
+      loseGame();
+    } else {
+      setClickedPokemon(prevClickedPokemon => new Set(prevClickedPokemon).add(id));
+
+      setPokemonData(prevData =>
+        prevData.map(data => {
+          if (id === data.id) {
+            return {
+              ...data,
+              isClicked: true
+            };
           }
-        shuffleCards();
+          return data;
+        })
+      );
 
+      changeScore();
+      shuffleCards();
     }
-    
-    function shuffleCards(){
-     setPokemonData(data => _.shuffle(data));
-    }
+  }
 
-    return (
-        <div className="pokemonDiv">
-          {pokemonData && pokemonData.map(pokemon => (
-            <div key={pokemon.id}>
-              <img className='pokemon' src={pokemon.imageUrl} onClick={()=>handleClick(pokemon.id,pokemon.isClicked)}  />
-            </div>
-          ))}
+  function shuffleCards() {
+    setPokemonData(data => _.shuffle(data));
+  }
+
+  return (
+    <div className="pokemonDiv">
+      {pokemonData && pokemonData.map(pokemon => (
+        <div key={pokemon.id}>
+          <img className='pokemon' src={pokemon.imageUrl} onClick={() => handleClick(pokemon.id)} />
         </div>
-    );
+      ))}
+    </div>
+  );
 }
